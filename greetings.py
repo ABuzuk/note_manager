1df125a880dd1db6dd706b923e263f49a368013b
ABuzuk <114508691+ABuzuk@users.noreply.github.com>
2024-12-24 11:28:45 +0300

    Update greetings.py
    with datetime

--- greetings.py
+++ greetings.py
@@ -1,18 +1,23 @@
 # Определяем переменные
 
-username = input ('Имя пользователя')
-title = input ('Заголовок заметки')
-content = input ('Описание заметки')
-status = input ('Статус заметки')
-created_date = input ('Дата создания заметки')
-expiry_date = input ('Дата истечения заметки')
+
+username = input ('Имя пользователя ')
+title = input ('Заголовок заметки ')
+content = input ('Описание заметки ')
+status = input ('Статус заметки ')
+from datetime import date
+current_date = date.today()
+created_date = print ('Дата создания заметки: ',current_date)
+
+expiry_date = input ('Дата истечения заметки: ')
 
 print ('Имя пользователя:', username)
 print ('Заголовок заметки:', title)
 print ('Описание заметки:', content)
 print ('Статус заметки:', status)
-print ('Дата создания заметки:', created_date)
+print ('Дата создания заметки: ' , current_date)
 print ('Дата истечения заметки:', expiry_date)
+
 
 
 
