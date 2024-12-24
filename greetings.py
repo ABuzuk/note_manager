fc76811f24d323d0fabe4f94a09f7084d9bcb4f7
ABuzuk <114508691+ABuzuk@users.noreply.github.com>
2024-12-24 12:55:56 +0300

    Update greetings.py
    с укороченной датой ввода

--- greetings.py
+++ greetings.py
@@ -7,17 +7,16 @@ content = input ('Описание заметки ')
 status = input ('Статус заметки ')
 from datetime import date
 current_date = date.today()
-created_date = print ('Дата создания заметки: ',current_date)
-
-expiry_date = input ('Дата истечения заметки: ')
+created_date = print ('Дата создания заметки: ', current_date)
+short_date = current_date.strftime ("%m.%d")
+expiry_date = input ('Дата истечения заметки')
 
 print ('Имя пользователя:', username)
 print ('Заголовок заметки:', title)
 print ('Описание заметки:', content)
 print ('Статус заметки:', status)
-print ('Дата создания заметки: ' , current_date)
+print ('Дата создания заметки: ' , short_date)
 print ('Дата истечения заметки:', expiry_date)
-
 
 
 
