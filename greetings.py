bfe1c1215fa93dbeadf55db39fcd525b512665cb
ABuzuk <114508691+ABuzuk@users.noreply.github.com>
2024-12-26 12:17:04 +0300

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -1,19 +1,18 @@
 # Определяем переменные
 
-username = input ('Имя пользователя ')
-title = input ('Заголовок заметки ')
-content = input ('Описание заметки ')
-status = input ('Статус заметки ')
-created_date = input ('Дата создания заметки:')
-expiry_date = input ('Дата истечения заметки:')
+username = input('Имя пользователя ')
+title = input('Заголовок заметки ')
+content = input('Описание заметки ')
+status = input('Статус заметки ')
+created_date = input('Дата создания заметки:')
+expiry_date = input('Дата истечения заметки:')
 
-print ('Имя пользователя:', username)
-print ('Заголовок заметки:', title)
-print ('Описание заметки:', content)
-print ('Статус заметки:', status)
-print ('Дата создания заметки: ' , created_date)
-print ('Дата истечения заметки:', expiry_date)
-
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки: ' , created_date)
+print('Дата истечения заметки:', expiry_date)
 
 
 
