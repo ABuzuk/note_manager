6160d2cc7966a8d6dce391d82571bf7710d83033
ABuzuk <114508691+ABuzuk@users.noreply.github.com>
2024-12-26 11:45:29 +0300

    Update greetings.py
    все 4 задания в одном коде с автоматической датой

--- greetings.py
+++ greetings.py
@@ -1,10 +1,9 @@
 # Определяем переменные
 
-
-username = input ('Имя пользователя ')
-title = input ('Заголовок заметки ')
-content = input ('Описание заметки ')
-status = input ('Статус заметки ')
+username = input ('Введите имя пользователя ')
+title = input ('Введите заголовок заметки ')
+content = input ('Введите описание заметки ')
+status = input("Введите статус заметки (например, 'Активна', 'Выполнена'): ")
 from datetime import date
 current_date = date.today()
 created_date = print ('Дата создания заметки: ',current_date)
@@ -12,13 +11,21 @@ short_date = current_date.strftime ("%m.%d")
 expiry_date = input ('Дата истечения заметки в формате гггг.мм.дд: ')
 short_expiry_date = expiry_date [6:11]
 
+# Создаем список заголовков заметки
+titles = []
+for i in range(3):
+    title = input(f"Введите заголовок заметки {i + 1}: ")
+    titles.append(title)
+
+
+print("\nВы ввели следующие данные:")
 print ('Имя пользователя:', username)
 print ('Заголовок заметки:', title)
 print ('Описание заметки:', content)
 print ('Статус заметки:', status)
 print ('Дата создания заметки: ' , short_date)
 print ('Дата истечения заметки:', short_expiry_date)
-
+print (titles)
 
 
 
